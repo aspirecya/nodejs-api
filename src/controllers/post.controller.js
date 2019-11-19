ece205714e1395489650c324886a9f49934ca044
wassim <x.liquid1@gmail.com>
2019-11-19 09:57:11 +0100

    crud post

---
+++ src/controllers/post.controller.js
@@ -0,0 +1,87 @@
+const Post = require('../models/post.model');
+const bcrypt = require('bcrypt');
+
+exports.create = (req, res) => {
+
+    const post = new Post({
+        title: req.body.title,
+        publishDate: req.body.publishDate,
+        author: req.body.author,
+        description: req.body.description,
+        id_user: req.body.id_user,
+    });
+
+    Post.save()
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message
+            })
+        })
+};
+
+exports.findAll = (req, res) => {
+    Post.find()
+        .then(posts => {
+            res.send(posts);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "An error has occured while fetching all posts."
+            })
+        })
+};
+
+exports.findById = (req, res) => {
+    Post.findById(_id = req.params.id)
+        .then(post => {
+            res.send(post)
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "An error has occured while fetching the post."
+            })
+        })
+};
+
+exports.findByIdAndUpdate = (req, res) => {
+    if(req.body.password = bcrypt.hashSync(req.body.password, 8));
+
+    Post.findByIdAndUpdate(req.params.id, req.body)
+        .then(post => {
+            res.send(post)
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "An error has occured while updating the post."
+            })
+        })
+};
+
+exports.findByIdAndRemove = (req, res) => {
+    Post.findByIdAndDelete(req.params.id)
+        .then(post => {
+            res.send(post);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "An error has occured while deleting the post."
+            })
+        })
+};
+
+exports.removeAllPosts = (req, res) => {
+    Post.remove()
+        .then(
+            res.status(200).send({
+                message: "Successfully deleted all posts."
+            })
+        )
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "An error has occured while deleting the post."
+            })
+        })
+};
---
+++ src/routes/post.route.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const post = require('../controllers/post.controller');
+const verifyAuth = require('../utils/verifyAuth');
+
+router.post('/posts/', post.create);
+router.delete('/posts/delete', verifyAuth, post.removeAllposts);
+router.get('/posts/', verifyAuth, post.findAll);
+router.get('/post/:id', verifyAuth, post.findById);
+router.patch('/post/:id', verifyAuth, post.findByIdAndUpdate);
+router.delete('/post/:id', verifyAuth, post.findByIdAndRemove);
+
+module.exports = router;
